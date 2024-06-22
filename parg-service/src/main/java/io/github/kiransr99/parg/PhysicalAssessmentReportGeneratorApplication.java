7b1372d6aaecdc4d405a9e6907f6501aa478780b
Kiran Shrestha <kiranxtha99@gmail.com>
2024-06-22 07:57:37 +0545

    backend project created

---
+++ parg-service/src/main/java/io/github/kiransr99/parg/PhysicalAssessmentReportGeneratorApplication.java
@@ -0,0 +1,13 @@
+package io.github.kiransr99.parg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicalAssessmentReportGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysicalAssessmentReportGeneratorApplication.class, args);
+	}
+
+}
---
+++ parg-service/src/test/java/io/github/kiransr99/parg/PhysicalAssessmentReportGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kiransr99.parg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicalAssessmentReportGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
