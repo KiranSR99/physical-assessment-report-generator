8c75598d125a4fad79c7691c43896cd79fdd744f
Deejs2 <jireldhiraj@123gmail.com>
2024-09-17 08:50:35 +0545

    physical report CRUD implement

--- parg-service/src/main/java/io/github/kiransr99/parg/dto/response/PhysicalReportResponse.java
+++ parg-service/src/main/java/io/github/kiransr99/parg/dto/response/PhysicalReportResponse.java
@@ -1,4 +1,33 @@
 package io.github.kiransr99.parg.dto.response;
 
+import io.github.kiransr99.parg.entity.PhysicalReport;
+import io.github.kiransr99.parg.entity.StudentEnrollment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PhysicalReportResponse {
+    private StudentEnrollment studentEnrollment;
+    private BigDecimal height;
+    private BigDecimal weight;
+    private BigDecimal bmi;
+
+    private String bmiLevel;
+    private String percentile;
+    private String comment;
+
+    public PhysicalReportResponse(PhysicalReport physicalReport) {
+        this.studentEnrollment = physicalReport.getStudentEnrollment();
+        this.height = physicalReport.getHeight();
+        this.weight = physicalReport.getWeight();
+        this.bmi = physicalReport.getBmi();
+        this.bmiLevel = physicalReport.getBmiLevel();
+        this.percentile = physicalReport.getPercentile();
+        this.comment = physicalReport.getComment();
+    }
 }
--- parg-service/src/main/java/io/github/kiransr99/parg/repository/PhysicalReportRepository.java
+++ parg-service/src/main/java/io/github/kiransr99/parg/repository/PhysicalReportRepository.java
@@ -2,6 +2,8 @@ package io.github.kiransr99.parg.repository;
 
 import io.github.kiransr99.parg.entity.PhysicalReport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PhysicalReportRepository extends JpaRepository<PhysicalReport, Integer> {
+@Repository
+public interface PhysicalReportRepository extends JpaRepository<PhysicalReport, Long> {
 }
