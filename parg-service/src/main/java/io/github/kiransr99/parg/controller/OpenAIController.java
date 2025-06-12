ba7285ab2ba6a5b6a13c35d0cf93bff2f0260727
Kiran Shrestha <kiranxtha99@gmail.com>
2025-06-12 22:04:32 +0545

    updating

--- parg-service/src/main/java/io/github/kiransr99/parg/controller/OpenAIController.java
+++ parg-service/src/main/java/io/github/kiransr99/parg/controller/OpenAIController.java
@@ -2,7 +2,7 @@ package io.github.kiransr99.parg.controller;
 
 
 import io.github.kiransr99.parg.controller.base.BaseController;
-import io.github.kiransr99.parg.service.OpenAIService;
+//import io.github.kiransr99.parg.service.OpenAIService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,13 +17,13 @@ import java.util.Map;
 @RequestMapping("/parg/api/v1/openai")
 public class OpenAIController extends BaseController {
 
-    private final OpenAIService openAIService;
-
-    @PostMapping("/generate")
-    public ResponseEntity<String> generateSummary(@RequestBody Map<String, String> request) {
-        String input = request.get("input");
-        String summary = openAIService.generateSummary(input);
-        return ResponseEntity.ok(summary);
-    }
+//    private final OpenAIService openAIService;
+//
+//    @PostMapping("/generate")
+//    public ResponseEntity<String> generateSummary(@RequestBody Map<String, String> request) {
+//        String input = request.get("input");
+//        String summary = openAIService.generateSummary(input);
+//        return ResponseEntity.ok(summary);
+//    }
 
 }
